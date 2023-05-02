1e162198e3abd2a467c1ea5eb502e701de8f71f8
isabelpaiva <isxpaiva@icloud.com>
2023-05-02 10:19:20 -0300

    feat: diagram

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,6 +5,8 @@ import {
   returnMovieSchema,
   returnList,
 } from "../schema/movieSchema";
+
+
 type IMovieRequest = z.infer<typeof moviesSchema>;
 type IMovieReturn = z.infer<typeof returnMovieSchema>;
 type IAllMoviesReturn = z.infer<typeof returnList>;
